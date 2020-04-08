ff7684b25b48bb14cd2db9bb286a1a0fa7b10233
Jorge Ramirez <joramirez96@gmail.com>
2020-04-08 00:57:54 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,20 @@ class Transfer
   end
 
   def execute_transaction
-    
+    if valid? && status == "pending"
+      if amount < self.sender.balance
+        self.sender.balance -= amount
+        self.receiver.balance += amount
+        self.status = "complete"
+      else
+         self.status = "rejected"
+         "Transaction rejected. Please check your account balance."
+      end
+    else
+       self.status = "rejected"
+       "Transaction rejected. Please check your account balance."
+    end
+  end
 
   def reverse_transfer
     if @status == "complete"
