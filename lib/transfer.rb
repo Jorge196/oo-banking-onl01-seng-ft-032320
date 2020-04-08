12e4ccf902c3c8fa4177c4a6229087169f45e20f
Jorge Ramirez <joramirez96@gmail.com>
2020-04-08 00:46:18 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,7 +2,7 @@ class Transfer
   # your code here
 
   require 'pry'
-class Transfer
+
   attr_accessor :sender, :receiver, :amount, :status
 
   def initialize(sender, receiver, amount)
@@ -25,7 +25,8 @@ class Transfer
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
-  
+  end
+
 
   def reverse_transfer
     if @status == "complete"
